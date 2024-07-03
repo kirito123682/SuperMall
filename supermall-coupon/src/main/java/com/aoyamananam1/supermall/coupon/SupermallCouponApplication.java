108ed00fefbfe432211e174eeaf0e82484d15d7a
AoyamaNanam1 <1458881379@qq.com>
2024-07-03 13:52:39 +0800

    项目初始结构

---
+++ supermall-coupon/src/main/java/com/aoyamananam1/supermall/coupon/SupermallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.aoyamananam1.supermall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermallCouponApplication.class, args);
+    }
+
+}
---
+++ supermall-product/src/main/java/com/aoyamananam1/supermall/product/SupermallProductApplication.java
@@ -0,0 +1,13 @@
+package com.aoyamananam1.supermall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermallProductApplication.class, args);
+	}
+
+}
